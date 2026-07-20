03f7cc8634de4d57546a9f4727a283a11bad2903
Vasanthi_Perla <84706043+Vasanthi2424@users.noreply.github.com>
2026-07-20 17:14:36 +0530

    Update mux_4_1.v

--- COMBINATIONAL_CKT/MUX/MUX_4_1/mux_4_1.v
+++ COMBINATIONAL_CKT/MUX/MUX_4_1/mux_4_1.v
@@ -1,1 +1,17 @@
-vhjjhh
+module mux4to1 (
+    input  [3:0] in,      
+// in[0]=I0, in[1]=I1, in[2]=I2, in[3]=I3
+    input  [1:0] sel,
+    output reg   y
+);
+    always @(*) begin
+        case (sel)
+            2'b00: y = in[0];
+            2'b01: y = in[1];
+            2'b10: y = in[2];
+            2'b11: y = in[3];
+            default: y = 1'bx;
+        endcase
+    end
+endmodule
+
